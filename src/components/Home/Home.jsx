ac68ca843c09f09fd52ea0e6d2b7d6ad851d53fb
alamin360am <alaminhimu0@gmail.com>
2023-05-04 02:46:37 +0600

    feature section complete with responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import "./home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCheck, faUtensilSpoon, faThumbTack, faSmile } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-    return (
-        <main>
-            <div className="banner">
-                <h1 className="title">All <span>Chinese Recipes</span> are here.</h1>
-                <button className="btn-primary">Explore More</button>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="banner">
+        <h1 className="title">
+          All <span>Chinese Recipes</span> are here.
+        </h1>
+        <button className="btn-primary">Explore More</button>
+      </div>
+      <section className="feature">
+        <h2 className="section-title">Our Features</h2>
+        <div className="grid col-4">
+          <div className="feature-card">
+            <FontAwesomeIcon icon={faUserCheck} className="icon" />
+            <p>90+ Chefs</p>
+          </div>
+          <div className="feature-card">
+            <FontAwesomeIcon icon={faUtensilSpoon} className="icon" />
+            <p>200+ Recipes</p>
+          </div>
+          <div className="feature-card">
+            <FontAwesomeIcon icon={faThumbTack} className="icon" />
+            <p>Easy Learning</p>
+          </div>
+          <div className="feature-card">
+            <FontAwesomeIcon icon={faSmile} className="icon" />
+            <p>Be Happy</p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
