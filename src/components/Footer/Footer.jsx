f5ac45f7b8fde21dc57a52ca080f6f4398a01625
alamin360am <alaminhimu0@gmail.com>
2023-05-04 01:35:10 +0600

    footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import './footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>footer</h1>
-        </div>
+        <footer>
+            <div className="footer-logo">
+                <img src="/Logo.png" alt="Logo" />
+            </div>
+            <div className="address">
+                <h2>Address</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dignissimos at modi consequatur amet aperiam debitis sapiente obcaecati iusto eligendi?</p>
+            </div>
+            <div className="link">
+                <Link to='/'>Home</Link>
+                <Link to='/blog'>Blogs</Link>
+                <a href="#">Contact Us</a>
+                <a href="#">About Us</a>
+                <a href="#">Terms and Conditions</a>
+            </div>
+        </footer>
     );
 };
 
