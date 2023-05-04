ccaba8fdc3bed2d07375390a4eaf36e938ce085d
alamin360am <alaminhimu0@gmail.com>
2023-05-04 18:07:19 +0600

    chef recipes bannar complete

--- src/components/ChefRecipes/ChefRecipes.jsx
+++ src/components/ChefRecipes/ChefRecipes.jsx
@@ -1,27 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import './ChefRecipes.css';
-import { useLoaderData, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./ChefRecipes.css";
+import { useLoaderData, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
 const ChefRecipes = () => {
-    const chefs = useLoaderData();
-    const chef_id = useParams().id;
-    
-    const [recipes, setRecipes] = useState([]);
+  const chefs = useLoaderData();
+  const chef_id = useParams().id;
 
-    useEffect(() => {
-        const chef = chefs.find((chef) => chef.chef_id == chef_id);
-        setRecipes(chef);
-      }, []);
+  const [recipes, setRecipes] = useState([]);
 
-      console.log(recipes);
+  useEffect(() => {
+    const chef = chefs.find((chef) => chef.chef_id == chef_id);
+    setRecipes(chef);
+  }, []);
 
-      const {chef_name, chef_photo, experience, likes, numbers_of_recipes} = recipes;
+  console.log(recipes);
 
-    return (
-        <div>
-            <a href="">recipe coming...</a>
+  const {
+    chef_name,
+    chef_photo,
+    experience,
+    likes,
+    numbers_of_recipes,
+    recipe,
+  } = recipes;
+
+  return (
+    <section className="chef-recipes">
+      <div className="chef-banner">
+        <div className="chef-card">
+          <div className="chef-img-box">
+            <img src={chef_photo} alt="" />
+          </div>
+          <h3 className="name">{chef_name}</h3>
+          <p className="experience">{experience}+ years experience</p>
+          <div className="likes">
+            <p className="recipe-number">
+              Recipes: <span>{numbers_of_recipes}</span>
+            </p>
+            <div className="like">
+              <FontAwesomeIcon icon={faThumbsUp} className="icon" />
+              <p>{likes}</p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default ChefRecipes;
