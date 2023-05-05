237770f1ccf0ef6725e0795dc0a688a926efd8a7
alamin360am <alaminhimu0@gmail.com>
2023-05-05 13:11:31 +0600

    environment variable created

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment', import.meta.env.VITE_PASS)
 const firebaseConfig = {
-  apiKey: "AIzaSyBwSydiGkEiHllGAcnRSnp-2Mt38ylvsdc",
-  authDomain: "chinise-food.firebaseapp.com",
-  projectId: "chinise-food",
-  storageBucket: "chinise-food.appspot.com",
-  messagingSenderId: "22772032335",
-  appId: "1:22772032335:web:f76787fce6780eea2c1b3a"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
